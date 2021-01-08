d56505d24f2ce0c11c30c67fe0f0a5bc8cc97b83
Mark Fabrizio <fabrizim@owlwatch.com>
2021-01-08 13:52:46 -0500

    Add plugin php file

---
+++ svil.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Svil - WP Library
+ * Plugin URI:        https://github.com/owlwatch/svil
+ * Description:       Library to ease WP development
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mark Fabrizio
+ * Author URI:        https://owlwatch.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       svil
+ * Domain Path:       /languages
+ */
+
+require __DIR__ . '/vendor/autoload.php';
