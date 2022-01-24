ed07faaf61c50a807b7fba5b5de6b9fff2bc4ac8
fabrizim <fabrizim@owlwatch.com>
2022-01-24 10:32:21 -0500

    Remove magic methods
    Magic methods were private to prevent cloning, but PHP 8 enforces those methods to be private, so lets just remove

--- lib/Util/SingletonTrait.php
+++ lib/Util/SingletonTrait.php
@@ -29,27 +29,4 @@ trait SingletonTrait
      */
     protected function __construct() {}
 
-    /**
-     * Private clone method to prevent cloning of the instance of the
-     * *Singleton* instance.
-     *
-     * @return void
-     */
-    private function __clone() {}
-
-    /**
-     * Private unserialize method to prevent unserializing of the *Singleton*
-     * instance.
-     *
-     * @return void
-     */
-    private function __wakeup() {}
-
-    /**
-     * Private unserialize method to prevent unserializing of the *Singleton*
-     * instance.
-     *
-     * @return void
-     */
-    private function __sleep() {}
 }
